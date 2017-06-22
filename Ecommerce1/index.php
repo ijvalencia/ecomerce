84926cad0b1b6c992e400ceaaa0ab7c85432bf1a
ijvalencia <jesusvalenciatrejo7@gmail.com>
2017-06-22 09:28:14 -0500

    avances

--- Ecommerce1/index.php
+++ Ecommerce1/index.php
@@ -1,17 +1,208 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>Ecommerce</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/body.css">
+        <link rel="stylesheet" href="css/acomodo.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">Portfolio</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                    
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <nav class="navbarmenu navbar-inverse">
+            <div class="container-fluid1">
+                <div class="navbar-header">
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                    <li><a href="#">Solo hoy</a></li>
+                    <li><a href="#">Promociones</a></li>
+                    <li><a href="#">Ofertas</a></li>
+                    <li><a href="#">cancelaciones</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div class="container-fluid bg-3 text-center">    
+            <h3></h3><br>
+            <div class="row">   
+                <div class="col-md-8"> 
+<!--                   -->
+                    <img id="superior1" src="IMG/fondo.jpg" class="img-responsive" style="width: 100%"  alt="Image">
+                    .col-md-8
+                    </div>
+                    <div class="col-md-4">
+                       <img id="superior2" src="IMG/fondo.jpg" class="img-responsive" style="width: 100%" alt="Image">
+                      .col-md-4
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                     <p>Some text..</p>
+                      <img src="IMG/fondo.jpg" class="img-responsive" style="width:100%" alt="Image">
+                         
+                    </div>
+                    <div class="col-md-4">
+                    <p>Some text..</p>
+                      <img src="IMG/fondo.jpg" class="img-responsive" style="width:100%" alt="Image"> 
+                        .col-md-4
+                    </div>
+                    <div class="col-md-4">  
+                        <p>Some text..</p>
+                         <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                        .col-md-4
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                         <p>Some text..</p>
+                           <img src="IMG/img.jpg" class="img-responsive" style="width:50%" alt="Image">           
+                        .col-md-6</div>
+                    <div class="col-md-6"> 
+                          <p>Some text..</p>
+                          <img src="IMG/img.jpg" class="img-responsive" style="width:50%" alt="Image">
+                        .col-md-6
+                    </div>
+                </div>
+        </div>
+        <br><br><br><br><br>
+        <div class="container-fluid bg-3 text-center">    
+             <h3></h3><br>
+            <div class="row">   
+                
+                <div class="col-md-8"> 
+                      <p>Some text..</p>
+                      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:50%" alt="Image">
+                    .col-md-8
+                </div>
+                <div class="col-md-4">
+                      <p>Some text..</p>
+                      <img class="superior" src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                      .col-md-4
+                </div>
+                
+               <!--<div class="col-sm-3"> 
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3"> 
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                -->
+            </div>
+        </div>
+        <br>
+        
+        <div class="jumbotron">
+            <div class="container text-center">
+                <div class="row">
+                    <h1>My Portfolio</h1>      
+                    <p>Some text that represents "Me"...</p>
+                </div>
+            </div>
+        </div>
+       <br>  
+     <br>
+        <!--carrusedel etiquetas los mas recomendados-->
+        <div class="row">
+            <div class="col-sm-3">
+                <label type="" class="btn btn-default">Normal</label>
+                <button type="button" class="btn btn-default">Normal</button>
+                <button type="button" class="btn btn-default">Normal</button>
+                <button type="button" class="btn btn-default">Normal</button>
+                <button type="button" class="btn btn-default">Normal</button>
+                <button type="button" class="btn btn-default">Normal</button>
+                <button type="button" class="btn btn-default">Normal</button>
+            <!--<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">-->
+            </div>
+        </div>
+        <!--fin de los etiquetas los mas recomendados-->
+        
+        <!--carrusel para abajo-->
+        <div class="jumbotron">
+            <div class="container text-center">
+                <div class="row">
+                    <h1>My Portfolio</h1>      
+                    <p>Some text that represents "Me"...</p>
+                </div>
+            </div>
+        </div>
+        <!--fin del carrusel-->
+        <div class="container-fluid bg-3 text-center">    
+            <h3>Some of my Work</h3><br>
+            <div class="row">
+                <div class="col-sm-3"> 
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3"> 
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+            </div>
+        </div>
+      <br>
+
+        <div class="container-fluid bg-3 text-center">    
+            <div class="row">
+                <div class="col-sm-3">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3"> 
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3"> 
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+                <div class="col-sm-3">
+                    <p>Some text..</p>
+                    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+                </div>
+            </div>
+        </div><br><br>
+
+        <footer class="container-fluid text-center">
+            <p>Footer Text</p>
+        </footer>
+
     </body>
 </html>
