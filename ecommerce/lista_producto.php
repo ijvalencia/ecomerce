b6f33eaafbfc7d9e52f65d0c02e211699f7f8221
Antdryem <t.antonandres@gmail.com>
2017-06-27 11:58:32 -0500

    27 junio
    productos registrados en la DB y menu de productos completo

--- ecommerce/lista_producto.php
+++ ecommerce/lista_producto.php
@@ -25,33 +25,32 @@
                     }
     ?>
  
-<frameset cols="20%,*" frameborder="no">
-<iframe></iframe>
-<iframe>
-                <table border='3' width='100%' align="right">
-                <tr><td valign="top" align="right"><a href="lista_producto.php">Categorias</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=accesorios">Accesorios</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=almacenamiento">Almacenamiento</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=audio y Video">Audio y Video</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=computadoras">Computadoras</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=videojuegos">Videojuegos</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=pantallas">Pantallas</a></td></tr>
-                <tr><td><a href="lista_producto.php?objeto=energia">Energia</a></td></tr>
+
+                
+
+<table border="0" width="100%">
+    <tr>
+        <td  valign="top" align='right' width="15%">
+    <table bgcolor="FF7F00"  border='0' width='45%' align="right">
+        <tr><td align="right"><a href="lista_producto.php"><img src="imagenes/categorias.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=accesorios"><img src="imagenes/accesorios.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=almacenamiento"><img src="imagenes/almacenaje.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=audio_y_Video"><img src="imagenes/AV.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=computadoras"><img src="imagenes/computadoras.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=videojuegos"><img src="imagenes/videojuegos.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=pantallas"><img src="imagenes/pantallas.png" style='width:100%;height:auto;'></a></td></tr>
+                <tr><td align="right"><a href="lista_producto.php?objeto=energia"><img src="imagenes/energia.png" style='width:100%;height:auto;'></a></td></tr>
                 
                 
                 </table>
-</iframe>
-</frameset>
-<table border="2" width="100%">
-    <tr>
-        <td align='right' width="20%">
-    
             
             
         </td>
+        
+        
             
         <td width='60%'>
-            <table border="2" width="100%">
+            <table valign="top" border="0" width="100%">
                 <?php 
                 
 
@@ -60,6 +59,8 @@
                 <tr>
                     <?php
                     for ($x=0;$x<=4;$x++){
+                        if(!empty($mostrar))
+                        {
                     ?>
                     
                     <td width="20%">
@@ -82,7 +83,7 @@
                     </td>
                 
                 
-                    <?php $mostrar= mysql_fetch_array($result);}?>
+                    <?php if($x!=4){$mostrar= mysql_fetch_array($result);}}}?>
                 
                 </tr>
                     
